ef9c6029ad552349783e4a01d1f8f9dc84c07bba
Emily Wagner <emilywagner598@gmail.com>
2019-01-18 23:36:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime? (num)
+  num_range = 2..(num/2)
+
+  if num < 2
+    return false
+  end
+
+  num_range.each do |tester|
+    if num % tester == 0
+      return false
+    end
+  end
+  true
+end
